bb9d54cc3a0d89b6f97c6d66f28c6cd565bf0b9a
Jefry Sánchez <sanchezmorajeffrey@yahoo.com>
2023-05-18 09:35:49 -0600

    change

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { filter } from 'rxjs';
+import { UserService } from '../services/user/user-service';
+import { User } from '../interfaces/user/user';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.sass']
 })
-export class UserComponent {
 
+export class UserComponent implements OnInit {
+  public usersService = inject(UserService);
+
+  public users = signal<User[]>([]);
+  public currentPage = signal(1);
+
+  public labelTotalUsers = computed(() => `Total de usuarios ${this.users().length}`);
+
+  ngOnInit(): void {
+    this.loadPage(this.currentPage());
+  }
+
+  loadPage(page: number) {
+    this.usersService.loadPage(page)
+      .pipe(
+        filter(users => users.length > 0)
+      )
+      .subscribe(newUsers => {
+        this.currentPage.set(page);
+        this.users.update(currentUsers => [...currentUsers, ...newUsers])
+      })
+  }
 }
